68e3056f63ef0b2a95c0e5289205c6a7021da98d
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-09 18:43:15 -0400

    set up app server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,28 @@
+require('dotenv').config()
 import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import createError from 'http-errors'
 
 const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+export const startServer = () => {
+  try {
+    app.listen(process.env.SERVER_PORT, () => {
+      console.log(
+        `Now listening on http://localhost:${process.env.SERVER_PORT}/`
+      )
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
