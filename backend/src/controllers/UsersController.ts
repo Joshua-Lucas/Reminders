94c03a2f8410b6b41035f478d4823fd43ad80465
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-13 17:00:42 -0400

    add create method to create new user

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,2 +1,28 @@
 import { logError } from '../utils/logError'
+import { StorageErrors } from '../utils/StorageErrors'
 import HttpStatus from 'http-status-codes'
+import { createNewUser, INewUser } from '../models/UserModel'
+// Types and Interfaces
+export interface ICreateNewUser extends INewUser {}
+
+export const create = async (req, res) => {
+  const body = req.body as ICreateNewUser
+
+  try {
+    const result = await createNewUser(body)
+    if (result.rowCount === 0) {
+      res
+        .status(HttpStatus.UNPROCESSABLE_ENTITY)
+        .send(StorageErrors.ALREADY_EXISTS)
+    } else {
+      res.status(HttpStatus.CREATED).json(result.rows[0])
+    }
+  } catch (error) {
+    logError(error)
+    res.sendStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  }
+}
+
+export const UsersController = {
+  create: create,
+}
