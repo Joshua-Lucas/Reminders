a51db9acdb98defa207df0da3f1745aefa12f817
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-15 19:01:45 -0400

    add errors messagees for login

--- backend/src/utils/StorageErrors.ts
+++ backend/src/utils/StorageErrors.ts
@@ -1,3 +1,5 @@
 export const StorageErrors = {
   ALREADY_EXISTS: 'The email has already been taken',
+  NO_ACCOUNT: 'Email does not match our records',
+  INVALID_PASSWORD: 'Password is incorrect',
 }
