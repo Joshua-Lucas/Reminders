c445504cf0536f491ba94414b4095ab07f5e2988
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-16 18:51:18 -0400

    create file to hold all frontend interfaces

---
+++ frontend/src/js/utils/interfaces.ts
@@ -0,0 +1,20 @@
+// REMINDER INTERFACES
+export interface IReminder {
+  id: number
+  title: string
+  details: string
+  daytobe: string
+  frequencey: number
+  user_id: number
+}
+
+export interface INewReminder {
+  title: IReminder['title']
+  details: IReminder['details']
+  daytobe: IReminder['daytobe']
+  frequencey: IReminder['frequencey']
+  user_id: IReminder['user_id']
+}
+
+export type GetReminders = IReminder[]
+export interface ICreateReminder extends INewReminder {}
