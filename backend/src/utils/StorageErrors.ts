423d711f64ba3aa9a02fd6af2f6c211b83841375
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-16 16:59:31 -0400

    Add error for when a reminder does not exist

--- backend/src/utils/StorageErrors.ts
+++ backend/src/utils/StorageErrors.ts
@@ -1,4 +1,7 @@
 export const StorageErrors = {
+  // reminders errors
+  NO_REMINDER: 'There are no reminders with that id',
+  // account errors
   ALREADY_EXISTS: 'The email has already been taken',
   NO_ACCOUNT: 'Email does not match our records',
   INVALID_PASSWORD: 'Password is incorrect',
