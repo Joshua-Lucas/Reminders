6b2866e5752e3213df278d338882e534e109d480
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-16 17:40:05 -0400

    set up interface for userdate and add create method

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -3,34 +3,38 @@ import { StorageErrors } from '../utils/StorageErrors'
 import HttpStatus from 'http-status-codes'
 import {
   createNewUser,
+  getUser,
+  updateUserInfo,
   INewUser,
   hashPass,
   IUser,
-  loginUser,
   compareCradentials,
+  IUserData,
 } from '../models/UserModel'
 
 // Types and Interfaces
 export interface ICreateNewUser extends INewUser {}
 
+//* CREATE AUTH VALIDATION MIDDLEWARE AND REFACTOR  */
 export const index = async (req, res) => {
   const body = req.body as IUser
   try {
     // querrying bd for user info
-    const getUser = await loginUser(body)
+    const userInfo = await getUser(body)
     // if no matching email fail
-    if (getUser.rowCount === 0) {
+    if (userInfo.rowCount === 0) {
       res.status(HttpStatus.UNAUTHORIZED).send(StorageErrors.NO_ACCOUNT)
     } else {
       // Check if the password entered matches password in db
-      const hashedPass = getUser.rows[0].password
+      const hashedPass = userInfo.rows[0].password
       const checkAuth = await compareCradentials(body.password, hashedPass)
       if (checkAuth === true) {
-        const userInfo = {
-          name: getUser.rows[0].name,
-          email: getUser.rows[0].email,
+        const userData: IUserData = {
+          id: userInfo.rows[0].id,
+          name: userInfo.rows[0].name,
+          email: userInfo.rows[0].email,
         }
-        res.status(HttpStatus.OK).json(userInfo)
+        res.status(HttpStatus.OK).json(userData)
       } else {
         res.status(HttpStatus.UNAUTHORIZED).send(StorageErrors.INVALID_PASSWORD)
       }
@@ -60,7 +64,34 @@ export const create = async (req, res) => {
   }
 }
 
+export const update = async (req, res) => {
+  const body = req.body as IUserData
+
+  try {
+    const result = await updateUserInfo(body)
+    const updatedUserInfo = {
+      id: result.rows[0].id,
+      name: result.rows[0].name,
+      email: result.rows[0].email,
+    }
+    res.status(HttpStatus.OK).json(updatedUserInfo)
+  } catch (error) {
+    logError(error)
+    res.sendStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  }
+}
+
+// export const destory = (req, res) => {
+//   const id: number = req.body.id
+
+//   try{
+
+//   }
+// }
+
 export const UsersController = {
   index: index,
   create: create,
+  update: update,
+  // destroy: destroy,
 }
