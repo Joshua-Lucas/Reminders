94e177d6d65be603c76f5f517babbe5dd2a910ed
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-16 18:15:51 -0400

    add clinent side code

---
+++ frontend/src/js/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "../css/App.css";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
