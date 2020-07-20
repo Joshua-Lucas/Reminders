76de01be50db37e0c8b1a569e0b34bc91126f42f
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-20 15:44:54 -0400

    map all fetch results to reminder component

--- frontend/src/js/components/Reminders.tsx
+++ frontend/src/js/components/Reminders.tsx
@@ -1,21 +1,36 @@
 import React, { useState, useEffect } from 'react'
-import { api } from '../utils/api/ReimdersApi'
-import { GetReminders } from '../utils/interfaces'
+import ReminderContainer from './Reminders/RemindersContiner'
+import { remindersApi } from '../utils/api/ReimdersApi'
+import { GetReminders, IReminder } from '../utils/interfaces'
 
 const Reminders: React.FC = ({}) => {
-  const [state, setState] = useState<GetReminders>()
+  const [state, setState] = useState<GetReminders>([])
 
   useEffect(() => {
-    const loadReminders = () => {
-      const result = api.get()
+    const loadReminders = async () => {
+      const result = await remindersApi.get()
       console.log(result)
+      setState(result)
     }
     loadReminders()
   }, [])
 
+  const filterdReminders = () => {
+    const all = state.map((reminders) => (
+      <ReminderContainer
+        key={reminders.id}
+        title={reminders.title}
+        details={reminders.details}
+        frequency={reminders.frequencey}
+      />
+    ))
+    return all
+  }
+
   return (
     <div>
-      <p>Test </p>
+      <h1>Your Reminders</h1>
+      {filterdReminders()}
     </div>
   )
 }
