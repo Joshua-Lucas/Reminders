4a6266b0c0154aed43700b838ee9710e27767ec9
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-20 17:17:01 -0400

    add a key and an onchange event and prop

--- frontend/src/js/utils/formComponents/Dropdown.tsx
+++ frontend/src/js/utils/formComponents/Dropdown.tsx
@@ -4,18 +4,28 @@ interface IDropdownProps {
   name: string
   labelName?: string
   options: string[]
+  value: string
+  event: (event: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
-const Dropdown: React.FC<IDropdownProps> = ({ name, labelName, options }) => {
+const Dropdown: React.FC<IDropdownProps> = ({
+  name,
+  labelName,
+  options,
+  value,
+  event,
+}) => {
   const selections = options.map((option) => (
-    <option value={option}>{option}</option>
+    <option key={option} value={option}>
+      {option}
+    </option>
   ))
 
   return (
     <React.Fragment>
       <label htmlFor={name}>
         {labelName} {/* This is option */}
-        <select name={name} id={name}>
+        <select name={name} id={name} onChange={event}>
           {selections}
         </select>
       </label>
