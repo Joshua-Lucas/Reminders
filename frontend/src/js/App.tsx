5fecc256dbcee274e3e01d8536ba7fc16b070fe9
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-21 11:55:57 -0400

    pull in tailwindcss, config and build it

--- frontend/src/js/App.tsx
+++ frontend/src/js/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import '../css/App.css'
 import Reminders from './components/Reminders/Reminders'
 
 function App() {
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
