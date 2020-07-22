55ab7d92aca55cc9af0670131069866971f2e6c8
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-21 21:22:32 -0400

    create useInput hook to use throughout code

---
+++ frontend/src/js/utils/formComponents/useInput.tsx
@@ -0,0 +1,35 @@
+import React, { useState, SetStateAction } from 'react'
+
+const useInput = (label: string, defaultSate: any, type: string) => {
+  const [state, setState] = useState(defaultSate)
+  const id = `use-dropdown-${label.replace(' ', '').toLowerCase()}`
+
+  // these props are to have flexability to style
+  type InputProps = {
+    inputStyle: string
+    labelStyle: string
+  }
+
+  const Input: React.FC<InputProps> = ({ inputStyle, labelStyle }) => (
+    <label htmlFor={id} className={labelStyle}>
+      {label}
+      <input
+        className={inputStyle}
+        type={type}
+        id={id}
+        name={label}
+        placeholder={`Enter ${label}`}
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        onBlur={(e) => setState(e.target.value)}
+      />
+    </label>
+  )
+  return [state, Input, setState] as [
+    string,
+    React.FC<InputProps>,
+    SetStateAction<string>
+  ]
+}
+
+export default useInput
