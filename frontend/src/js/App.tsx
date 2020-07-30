3491716031663538cbfdc51f02a66a52da9d42f3
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-30 18:18:08 -0400

    wrapped app with ThemeProvider to access the theme

--- frontend/src/js/App.tsx
+++ frontend/src/js/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Reminders from './components/Reminders/Reminders'
+import { ThemeProvider } from 'styled-components'
+import Theme from './utils/Theme'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Reminders />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={Theme}>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Reminders />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   )
 }
 
