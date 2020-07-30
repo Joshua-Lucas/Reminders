c230beaa41c87fd02dba84d426876c0d53e5fc2f
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-30 18:17:37 -0400

    Add colors and theme for app

---
+++ frontend/src/js/utils/Theme/Colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  accent: '#e63946',
+  neutral: {
+    light: '#F1FAEE',
+    dark: '#1D3557',
+  },
+  primary: {
+    light: '#A8DADC',
+    dark: '#457B9D',
+  },
+}
+
+export default colors
---
+++ frontend/src/js/utils/Theme/index.ts
@@ -0,0 +1,22 @@
+import colors from './Colors'
+
+export const Theme = {
+  /*-- COLORS --*/
+
+  // Brand Colors
+  primaryColor: colors.primary.light,
+  secondaryColor: colors.primary.dark,
+  tirtiaryColor: colors.accent,
+  lightNeutralColor: colors.neutral.light,
+  darkNeutralColor: colors.neutral.dark,
+  // Text Color
+
+  primaryTextColor: colors.neutral.dark,
+  secondarydTextColor: colors.neutral.light,
+
+  // Button Colors
+  primaryHoverColor: colors.primary.dark,
+  secondaryHoverColor: '#fff',
+}
+
+export default Theme
