3608b510be4b6deda4e5a21bbe66df25b8d78cef
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-06 18:07:14 -0400

    Setup grid layout for app

--- frontend/src/js/App.tsx
+++ frontend/src/js/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import Reminders from './components/Reminders/Reminders'
+import styled from 'styled-components'
 import { ThemeProvider } from 'styled-components'
 import Theme from './utils/Theme'
+import Reminders from './components/Reminders/Reminders'
+import SideBar from './components/Navigation/SideBar'
+import MainNav from './components/Navigation/MainNav'
+
+const AppWrapper = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 15% 70% 15%;
+  grid-template-rows: 10% 90%;
+`
 
 function App() {
   return (
@@ -10,7 +20,13 @@ function App() {
       <Router>
         <Switch>
           <Route path="/">
-            <Reminders />
+            <AppWrapper>
+              <MainNav />
+              <SideBar
+                setFilter={['Sunday', 'Monday', 'Tuesday', 'Wednesday']}
+              />
+              <Reminders />
+            </AppWrapper>
           </Route>
         </Switch>
       </Router>
