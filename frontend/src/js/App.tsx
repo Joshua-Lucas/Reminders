b29d0f47d9b1d81348f5df4315b9f6833f4ff15a
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-11 19:08:02 -0400

    add Global styles and refactored

--- frontend/src/js/App.tsx
+++ frontend/src/js/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { ThemeProvider } from 'styled-components'
 import Theme from './utils/Theme'
+import GlobalStyle from './utils/Theme/Global'
 import Reminders from './components/Reminders/Reminders'
 import SideBar from './components/Navigation/SideBar'
 import MainNav from './components/Navigation/MainNav'
 import { ReminderContextProvider } from './context/RemindersContext'
+import { filterOptions } from './utils/interfaces'
 
 const AppWrapper = styled.div`
   height: 100vh;
@@ -24,10 +26,9 @@ function App() {
             <Route path="/">
               <AppWrapper>
                 <MainNav />
-                <SideBar
-                  setFilter={['Sunday', 'Monday', 'Tuesday', 'Wednesday']}
-                />
+                <SideBar setCategories={filterOptions} />
                 <Reminders />
+                <GlobalStyle />
               </AppWrapper>
             </Route>
           </Switch>
