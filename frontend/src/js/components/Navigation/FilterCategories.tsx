c76951ed90ebb1f529581f6ed5cad9047a46021e
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-11 19:10:51 -0400

    update style and add event to Prop interface

--- frontend/src/js/components/Navigation/FilterCategories.tsx
+++ frontend/src/js/components/Navigation/FilterCategories.tsx
@@ -3,17 +3,22 @@ import styled from 'styled-components'
 import { Icons, UtilityStyles } from '@jludev/component-lib-typescript'
 
 //Styled Components
-const FilterCatContainer = styled.div`
+const FilterCatContainer = styled.button`
   display: flex;
   align-items: center;
+  margin: 1rem 0;
   padding-left: 0.5rem;
+  background-color: inherit;
+  cursor: pointer;
+  border: none;
 `
-const FilterTitle = styled.h2`
+const FilterTitle = styled.h5`
+  margin: 0;
   padding: 0 1rem;
+  font-weight: normal;
 `
 const ReminderIcon = styled.svg`
-  height: 20px;
-  width: 20px;
+  width: 2rem;
   padding: 0.5rem;
   background-color: ${(props) => props.theme.primaryColor};
   border-radius: ${UtilityStyles.borderRadius.full};
@@ -21,17 +26,23 @@ const ReminderIcon = styled.svg`
 
 //Interfaces
 interface IFilterCategoriesProps {
+  id: string
   filterCategory: string
+  event:
+    | ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void)
+    | undefined
 }
 
 //React Component
 const FilterCategories: React.FC<IFilterCategoriesProps> = ({
+  id,
   filterCategory,
+  event,
 }) => {
   return (
-    <FilterCatContainer>
+    <FilterCatContainer onClick={event}>
       <ReminderIcon viewBox="0 0 20 20">{Icons.BulletdList}</ReminderIcon>
-      <FilterTitle>{filterCategory}</FilterTitle>
+      <FilterTitle id={id}>{filterCategory}</FilterTitle>
     </FilterCatContainer>
   )
 }
