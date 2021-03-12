4704fd9dd8f4bc09acf879926695724a12d6cb36
Joshua Lucas <joshuamlucas91@gmail.com>
2021-03-12 16:04:08 -0500

    update styling

--- frontend/src/js/components/Navigation/MainNav.tsx
+++ frontend/src/js/components/Navigation/MainNav.tsx
@@ -6,12 +6,24 @@ const NavContainer = styled.div`
   grid-column: 1/4;
   grid-row: 1/2;
   border-bottom: 2px solid ${(props) => props.theme.lightNeutralColor};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 3rem;
 `
 //Interfaces
 interface IMainNavProps {}
 
 //React Component
 const MainNav: React.FC<IMainNavProps> = ({}) => {
-  return <NavContainer></NavContainer>
+  return (
+    <NavContainer>
+      <div>
+        <h1>Reminders</h1>
+      </div>
+      <div>
+        <a>Account</a>
+      </div>
+    </NavContainer>
+  )
 }
 export default MainNav
